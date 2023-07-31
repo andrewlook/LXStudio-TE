1615460ca4b9f6682ae8fe27373ef818a7d7d7b8
Andrew Look <look@aesthetic.com>
2023-07-30 21:06:05 -0400

    wip: EMA

--- src/main/java/titanicsend/pattern/look/SigmoidDanceAveraged.java
+++ src/main/java/titanicsend/pattern/look/SigmoidDanceAveraged.java
@@ -7,6 +7,7 @@ import titanicsend.pattern.yoffa.effect.NativeShaderPatternEffect;
 import titanicsend.pattern.yoffa.framework.PatternTarget;
 import titanicsend.pattern.yoffa.framework.TEShaderView;
 import titanicsend.pattern.yoffa.shader_engine.NativeShader;
+import titanicsend.util.TEMath;
 
 @LXCategory("Look Shader Patterns")
 public class SigmoidDanceAveraged extends TEPerformancePattern {
@@ -28,10 +29,13 @@ public class SigmoidDanceAveraged extends TEPerformancePattern {
                 new PatternTarget(this));
     }
 
-    private float minLow = Float.POSITIVE_INFINITY;
-    private float maxLow = Float.NEGATIVE_INFINITY;
-    private float minHigh = Float.POSITIVE_INFINITY;
-    private float maxHigh = Float.NEGATIVE_INFINITY;
+    private float minLo = Float.POSITIVE_INFINITY;
+    private float maxLo = Float.NEGATIVE_INFINITY;
+    private float minHi = Float.POSITIVE_INFINITY;
+    private float maxHi = Float.NEGATIVE_INFINITY;
+    private TEMath.EMA emaLo = new TEMath.EMA(0., .01);
+    private TEMath.EMA emaHi = new TEMath.EMA(0., .01);
+
 
 
     @Override
@@ -40,41 +44,45 @@ public class SigmoidDanceAveraged extends TEPerformancePattern {
         int halfNBands = fullNBands / 2;
 
         // TODO: EMA?
-        float avgLow = eq.getAveragef(0, halfNBands);
-
-        float avgHigh = eq.getAveragef(halfNBands, fullNBands - halfNBands);
-
+        float curLo = eq.getAveragef(0, halfNBands);
+        float curHi = eq.getAveragef(halfNBands, fullNBands - halfNBands);
+        emaLo.update(curLo, deltaMs);
+        emaHi.update(curHi, deltaMs);
         // TODO: keep max/min on a moving window?
-        if (avgLow < minLow) {
-            minLow = avgLow;
+        if (curLo < minLo) {
+            minLo = curLo;
         }
-        if (avgLow > maxLow) {
-            maxLow = avgLow;
+        if (curLo > maxLo) {
+            maxLo = curLo;
         }
-        if (avgHigh < minHigh) {
-            minHigh = avgHigh;
+        if (curHi < minHi) {
+            minHi = curHi;
         }
-        if (avgHigh > maxHigh) {
-            maxHigh = avgHigh;
+        if (curHi > maxHi) {
+            maxHi = curHi;
         }
+
+        float runningAvgLo = emaLo.getValuef();
+        float runningAvgHi = emaHi.getValuef();
 
         System.out.printf("fullNBands = %s, halfNBands = %s\n", fullNBands, halfNBands);
-        System.out.printf("avgLow = %s, avgHigh = %s\n", avgLow, avgHigh);
+        System.out.printf("curLo = %s, curHi = %s\n", curLo, curHi);
+        System.out.printf("runningAvgLo = %s, runningAvgHi = %s\n", runningAvgLo, runningAvgHi);
 
-        float normLow = avgLow / (maxLow - minLow);
-        float normHigh = avgHigh / (maxHigh - minHigh);
-        System.out.printf("normLow = %s, normHigh = %s\n", normLow, normHigh);
+        float normLo = curLo / (maxLo - minLo);
+        float normHi = curHi / (maxHi - minHi);
+        System.out.printf("normLo = %s, normHi = %s\n", normLo, normHi);
 
-        float scaledLow = normLow * 2 - 1;
-        float scaledHigh = normHigh * 2 - 1;
-        System.out.printf("scaledLow = %s, scaledHigh = %s\n", scaledLow, scaledHigh);
+        float scaledLo = normLo * 2 - 1;
+        float scaledHi = normHi * 2 - 1;
+        System.out.printf("scaledLo = %s, scaledHi = %s\n", scaledLo, scaledHi);
 
-//        shader.setUniform("iWow1", avgLow);
-//        shader.setUniform("iWow2", avgHigh);
-//        shader.setUniform("iWow1", normLow);
-//        shader.setUniform("iWow2", normHigh);
-        shader.setUniform("iWow1", scaledLow);
-        shader.setUniform("iWow2", scaledHigh);
+//        shader.setUniform("iWow1", avgLo);
+//        shader.setUniform("iWow2", avgHi);
+//        shader.setUniform("iWow1", normLo);
+//        shader.setUniform("iWow2", normHi);
+        shader.setUniform("iWow1", scaledLo);
+        shader.setUniform("iWow2", scaledHi);
 
         // run the shader
         effect.run(deltaMs);
@@ -89,5 +97,4 @@ public class SigmoidDanceAveraged extends TEPerformancePattern {
         effect.onActive();
         shader = effect.getNativeShader();
     }
-
 }
